dd86b705f0d817f454a0b75724dfecc595008470
matinnasirlahijani <matinnasirlahijani@gmail.com>
2023-08-02 15:42:09 +0330

    report views added

---
+++ resources/views/report/edit.blade.php
@@ -0,0 +1,17 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title> ویرایش گزارش</title>
+    @include('./components/head')
+    @section('./components/head')
+</head>
+<body dir="rtl">
+    <div>
+
+        @include('./components/header')
+        @section('./components/header')
+    </div>
+</body>
+</html>
