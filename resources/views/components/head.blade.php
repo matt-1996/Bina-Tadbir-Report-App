a1c95d51f33e7c17a58348d7a27f4bf5407d2d2d
matinnasirlahijani <matinnasirlahijani@gmail.com>
2023-08-02 15:46:28 +0330

    head added

---
+++ resources/views/components/head.blade.php
@@ -0,0 +1,13 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    @yield('head')
+    <link rel="icon" href="{{asset('img/logo.jpg')}}" type="image/x-icon">
+    @vite('resources/css/app.css')
+</head>
+<body>
+
+</body>
+</html>
