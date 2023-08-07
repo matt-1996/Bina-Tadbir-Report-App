fc8e7576c5f8c10e60824f4c184cd77fa50778f1
matinnasirlahijani <matinnasirlahijani@gmail.com>
2023-08-07 19:17:49 +0330

    routes updated

--- routes/web.php
+++ routes/web.php
@@ -22,39 +22,68 @@ use App\Http\Controllers\ticketController;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+
+Route::get('/' , [loginController::class , 'index'])->name('root');
+
+Route::post('login' , [loginController::class , 'login'])->name('login');
+
+Route::group(['middleware' =>['auth', 'isAdmin']], function(){
+    Route::get('dashboard' , [adminController::class , 'index'])->name('dashboard');
 });
 
-Route::get('/' , [loginController::class , 'index']);
+Route::group(['middleware' => ['auth']], function(){
 
-Route::get('register' , [registerController::class , 'index'])->name('client.add');
 
-Route::get('dashboard' , [adminController::class , 'index'])->name('dashboard');
+    Route::get('register' , [registerController::class , 'index'])->name('client.add');
 
-Route::get('client' , [clientController::class , 'index'])->name('client.index');
+    Route::get('client' , [clientController::class , 'index'])->name('client.index');
 
-Route::get('client/edit' , [clientController::class , 'edit'])->name('client.edit');
+    Route::get('client/search/{name}' , [clientController::class , 'searchUsers'])->name('client.search');
 
-Route::get('report' , [reportController::class , 'index'])->name('report.index');
+    Route::get('client/edit' , [clientController::class , 'edit'])->name('client.edit');
 
-Route::get('report/single' , [reportController::class , 'single'])->name('report.single');
+    Route::get('client/profile' , [clientController::class , 'profile'])->name('client.profile');
 
-Route::get('report/edit' , [reportController::class , 'edit'])->name('report.edit');
+    Route::get('report' , [reportController::class , 'index'])->name('report.index');
 
-Route::get('report/request' , [reportController::class , 'request'])->name('report.request');
+    Route::get('report/add', [reportController::class , 'add'])->name('report.add');
 
-Route::get('profile' , [profileController::class , 'index'])->name('profile.index');
+    Route::get('report/getAgentById/{id}', [reportController::class , 'getAgentById'])->name('getAgentById');
 
-Route::get('profile/edit' , [profileController::class , 'edit'])->name('profile.edit');
+    Route::post('report/form/new', [reportController::class , 'new'])->name('report.form.new');
 
-Route::get('ticket' , [ticketController::class , 'index'])->name('ticket.index');
+    Route::get('report/single' , [reportController::class , 'single'])->name('report.single');
 
-Route::get('ticket/single' , [ticketController::class , 'single'])->name('ticket.single');
+    Route::get('report/edit' , [reportController::class , 'edit'])->name('report.edit');
 
-Route::get('ticket/new' , [ticketController::class , 'new'])->name('ticket.new');
+    Route::get('report/edit/single/{id}' , [reportController::class , 'editSingle'])->name('report.edit.single');
 
-route::get('logout' , [loginController::class , 'logout'])->name('logout');
+    Route::post('report/update' , [reportController::class , 'update'])->name('report.update');
+
+    Route::get('report/searchClient/{name}' , [reportController::class , 'searchClient'])->name('report.searchClient');
+
+    Route::get('report/request' , [reportController::class , 'request'])->name('report.request');
+
+    Route::get('report/delete/{id}' , [reportController::class , 'destroy'])->name('report.delete');
+
+    Route::get('profile' , [profileController::class , 'index'])->name('profile.index');
+
+    Route::get('profile/edit' , [profileController::class , 'edit'])->name('profile.edit');
+
+    Route::get('ticket' , [ticketController::class , 'index'])->name('ticket.index');
+
+    Route::get('ticket/single' , [ticketController::class , 'single'])->name('ticket.single');
+
+    Route::get('ticket/new' , [ticketController::class , 'new'])->name('ticket.new');
+
+    route::get('logout' , [loginController::class , 'logout'])->name('logout');
+});
+
+
+
+
+
+
 
 
 
