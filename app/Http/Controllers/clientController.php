6088f62976ba5ad51427d1e2121742e3413cb2d5
matinnasirlahijani <matinnasirlahijani@gmail.com>
2023-08-07 19:17:36 +0330

    client crud updated

--- app/Http/Controllers/clientController.php
+++ app/Http/Controllers/clientController.php
@@ -3,12 +3,36 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\user;
 
 class clientController extends Controller
 {
     public function index()
     {
-        return view('client.index');
+        $users = user::all();
+
+        return view('client.index', compact('users'));
+    }
+
+    public function searchUsers($name)
+    {
+        if($name == 'all')
+        {
+            $users = user::all();
+        }else{
+
+            $users = user::where('name', $name)->get();
+        }
+
+        $returnHTML = view('ajaxComponents.UsersTable', compact('users'))->render();
+
+       return response()->json(array('success' => true, 'html' => $returnHTML));
+    }
+
+    public function profile()
+    {
+        $user = auth()->user();
+        return view('client.profile', compact('user'));
     }
 
     public function addView()
@@ -21,9 +45,11 @@ class clientController extends Controller
 
     }
 
-    public function edit()
+    public function edit(Request $request)
     {
-        return view('client.edit');
+        $user = user::where('id', $request->id)->first();
+
+        return view('client.edit', compact('user'));
     }
 
     public function update()
