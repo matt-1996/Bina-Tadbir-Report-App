a93fd6fd66c44939c778f08cc6fb0eb00ccdd7c1
matinnasirlahijani <matinnasirlahijani@gmail.com>
2023-08-07 19:17:21 +0330

    admin crud update

--- app/Http/Controllers/adminController.php
+++ app/Http/Controllers/adminController.php
@@ -3,11 +3,18 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-
+use Illuminate\Support\Facades\Auth;
+use App\Models\report;
+use Hekmatinasser\Verta\Verta;
 class adminController extends Controller
 {
-    public function index()
+    public function index(Request $request)
     {
-        return view('admin.dashboard');
+        $verta = new Verta;
+        $reports = report::where('month' , '=', $verta->month)
+                    ->orderBy('created_at' , 'desc')
+                    ->get();
+
+        return view('admin.dashboard', compact('reports'));
     }
 }
